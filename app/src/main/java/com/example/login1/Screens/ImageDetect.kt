c916427ed3fc5d13fd19bcb376c51aaaf967260d
Venkata Sai Pramod <venkatasaipramod.tadavarthi@sasken.com>
2023-09-28 12:49:40 +0530

    Adding ui for 3 items in navigation drawer

--- app/src/main/java/com/example/login1/Screens/ImageDetect.kt
+++ app/src/main/java/com/example/login1/Screens/ImageDetect.kt
@@ -1,10 +1,20 @@
 package com.example.login1.Screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalDrawerSheet
@@ -16,10 +26,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.login1.Components.AppTopBar
 import com.example.login1.Components.NavigationDrawerBody
 import com.example.login1.Components.NavigationDrawerHeader
@@ -84,14 +107,61 @@ fun ImageScreen(
         ){
 
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "ImageDetect Page content comes here.")
+                modifier = Modifier
+                    .fillMaxSize()
+                    .absolutePadding(50.dp, 100.dp, 50.dp, 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                Text(
+                    text = "Image Detect", style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Image(painter = painterResource(id = R.drawable.baseline_image_search_24),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(200.dp))
+                Spacer(modifier = Modifier.heightIn(10.dp))
+                Row {
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .heightIn(20.dp),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text = "Gallery")
+                        }
+
+                    }
+                    Spacer(modifier = Modifier.width(80.dp))
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .heightIn(20.dp),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text = "Camera")
+                        }
+
+                    }
+                }
             }
         }
 
     }
 
+}
 
+@Preview
+@Composable
+fun ImageScreenPreview(){
+    ImageScreen(navController = rememberNavController())
 }
--- app/src/main/java/com/example/login1/Screens/LiveDetect.kt
+++ app/src/main/java/com/example/login1/Screens/LiveDetect.kt
@@ -1,10 +1,19 @@
 package com.example.login1.Screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalDrawerSheet
@@ -17,7 +26,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.login1.Components.AppTopBar
@@ -84,10 +99,52 @@ fun LiveScreen(
         ){
 
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "Live Page content comes here.")
+                modifier = Modifier
+                    .fillMaxSize()
+                    .absolutePadding(50.dp, 100.dp, 50.dp, 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Live Video Detect", style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Image(painter = painterResource(id = R.drawable.baseline_live_tv_24),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(200.dp))
+                Spacer(modifier = Modifier.heightIn(10.dp))
+                Row {
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .heightIn(20.dp),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text = "Gallery")
+                        }
+
+                    }
+                    Spacer(modifier = Modifier.width(80.dp))
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ){
+                        Box(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .heightIn(20.dp),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text = "Camera")
+                        }
+
+                    }
+                }
             }
         }
 
